cdcfdb9a3f9dc73020adc955c7fb146331b179f3
Lephare9 <andersvirum@gmail.com>
2026-05-01 22:40:47 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def analyze_image_with_ai(image_bytes):
         print("🔥 CALLING GEMINI...")
 
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-1.5-pro-latest",
             contents=[
                 types.Content(
                     parts=[
