8757b00118b18d5c28e3bb75d50ddedddc4d82c0
Lephare9 <andersvirum@gmail.com>
2026-05-01 11:47:55 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ Ingen forklaring.
 """
 
         response = client.models.generate_content(
-            model="gemini-1.5-flash",
+            model="gemini-1.5-flash-002",
             contents=types.Content(
                 role="user",
                 parts=[
