734881452c27b8d4de47c86fae8837937a690240
Lephare9 <andersvirum@gmail.com>
2026-05-01 21:10:50 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -33,7 +33,7 @@ def analyze_image_with_ai(image_bytes):
         print("🔥 CALLING AI (HTTP)...")
 
         response = requests.post(
-            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-vision:generateContent,
+            "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-pro-vision:generateContent",
             headers={"Content-Type": "application/json"},
             params={"key": os.getenv("GEMINI_API_KEY")},
             json={
