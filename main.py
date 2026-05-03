0574fbbdc775c2d3f21a7d25d41aa9be0f9c94b1
Lephare9 <andersvirum@gmail.com>
2026-05-03 21:09:21 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -50,7 +50,7 @@ def analyze_image(image_bytes):
         """
 
         response = client.models.generate_content(
-            model="gemini-2.0-flash",
+            model="gemini-1.5-flash-latest",
             contents=[
                 {"text": prompt},
                 {
