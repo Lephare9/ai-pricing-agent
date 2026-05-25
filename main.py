82962134a64b0dfadf732c499dced35c220c51b0
Lephare9 <andersvirum@gmail.com>
2026-05-25 16:05:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ genai.configure(
     api_key=os.getenv("GEMINI_API_KEY")
 )
 
-model = genai.GenerativeModel("gemini-1.5-flash")
+model = genai.GenerativeModel("gemini-2.5-flash")
 
 # ---------------------------------------------------
 # BLACKLIST
